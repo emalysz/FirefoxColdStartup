c7354666da0c9d7cbc302ae7e33637ebf1cb3d48
Doug Thayer <dougathayer@gmail.com>
2020-05-07 09:07:47 -0700

    Edits to GraphProfileData.py
    Sorry these aren't more atomic. This does a few things:
    - Make it so that we don't have to change anything in this file if
      we change which builds we're testing, even if we have more than
      two of such builds.
    - Graph a histogram of the data, which can sometimes make it
      clearer what the change is.
    - Print t-test results for the data, so that we can have some
      kind of confidence measure that what we're seeing isn't just
      due to chance.

--- GraphProfileData.py
+++ GraphProfileData.py
@@ -2,9 +2,14 @@ import os
 import lz4.block
 import lz4.frame
 import json
+import statistics
 import time
+import re
 import matplotlib.pyplot as plt
+from datetime import datetime
 from pathlib import Path
+from scipy.stats import ttest_ind
+from sets import Set
 
 home = str(Path.home())
 pathToExperimentFolder = home + '\\Experiment'
@@ -19,49 +24,83 @@ firstPaintAboutHomeList = []
 visibleReadBytes = []
 isCold = []
 
+colors = [
+	'#e69f00',
+	'#56b4e9',
+	'#b4e956',
+	# TODO: add more colors as necessary
+]
+
+builds = []
+buildSet = Set()
+
 class DataPoint:
-	def __init__(self, timeStamp, time, isControl):
-		self.timeStamp = timeStamp
+	def __init__(self, timeStamp, time, label):
+		self.timeStamp = datetime.strptime(timeStamp[0:19], "%Y-%m-%dT%H:%M:%S")
 		self.time = time
-		self.isControl = isControl
+		self.label = label
 
 # Creates DataPoint object from the json file. 
-def addDataPoints(data, isControl):
+def addDataPoints(data, label):
 	# Get the time stamp of this profile
 	time = data['creationDate']
 
 	# First paint measure
 	firstPaint = data['payload']['simpleMeasurements']['firstPaint']
-	firstPaintDataPoint = DataPoint(time, firstPaint, isControl)
+	firstPaintDataPoint = DataPoint(time, firstPaint, label)
 	firstPaintList.append(firstPaintDataPoint)
 
 	# About home top sites first paint
 	aboutHomeFirstPaint = data['payload']['processes']['parent']['scalars']['timestamps.about_home_topsites_first_paint']
-	firstPaintAboutHomeDataPoint = DataPoint(time, aboutHomeFirstPaint, isControl)
+	firstPaintAboutHomeDataPoint = DataPoint(time, aboutHomeFirstPaint, label)
 	firstPaintAboutHomeList.append(firstPaintAboutHomeDataPoint)
 
 	readBytes = data['payload']['simpleMeasurements']['startupWindowVisibleReadBytes']
-	readBytesDataPoint = DataPoint(time, readBytes, isControl)
+	readBytesDataPoint = DataPoint(time, readBytes, label)
 	visibleReadBytes.append(readBytesDataPoint)
 
-	isColdResults = data['payload']['processes']['parent']['scalars']['startup.is_cold']
-	isColdDataPoint = DataPoint(time, isColdResults, isControl)
+	isColdResults = data['payload']['processes']['parent']['scalars'].get('startup.is_cold', 0)
+	isColdDataPoint = DataPoint(time, isColdResults, label)
 	isCold.append(isColdDataPoint)
 
 
 # Graphs the data from the given list. yAxis is an optional parameter that defaults to time.
-def graphData(list, title, yAxis = "Time (ms)"):
-	list.sort(key=lambda x: x.timeStamp, reverse=False)
-	for data in list:
-		if data.isControl:
-			plt.plot(data.timeStamp, data.time, 'bo')
-		else:
-			plt.plot(data.timeStamp, data.time, 'yo')
-	plt.xlabel("Trial")
-	plt.ylabel(yAxis)
-	plt.title(title)
-	plt.xticks(rotation=90)
-	plt.tight_layout()
+def graphData(dataset, title, showHistogram, yAxis = "Time (ms)"):
+	dataset.sort(key=lambda x: x.timeStamp, reverse=False)
+
+	datasets = {}
+	for build in builds:
+		datasets[build['label']] = [x.time for x in dataset if x.label == build['label']]
+
+	for build in builds:
+		label = build['label']
+		color = build['color']
+		if len(datasets[label]) == 0:
+			continue
+		print("Mean for %s: %f" % (label, statistics.mean(datasets[label])))
+		print("T-Test result: ")
+		print(ttest_ind(datasets['control'], datasets[label]))
+
+		plt.plot(
+			[x.timeStamp for (i, x) in enumerate(dataset) if x.label == label],
+			[x.time for x in dataset if x.label == label],
+			'o',
+			color = color,
+			label = label)
+
+	plt.legend()
+	plt.show()
+
+	if not showHistogram:
+		return
+
+	plt.hist([
+			[x.time for x in dataset if x.label == build['label']] for build in builds
+		], density = True,
+		color = [build['color'] for build in builds],
+		label = [build['label'] for build in builds],
+		bins = 10)
+	plt.legend()
 	plt.show()
 
 
@@ -71,9 +110,12 @@ for filename in os.listdir(pathToExperimentFolder):
 		fileLocation = pathToExperimentFolder + '\\' + filename
 		
 		# See if it is a control or test build
-		isControl = False
-		if 'control' in filename:
-			isControl = True
+		match = re.search(r"([a-z_]+)_[0-9]+", filename);
+		buildLabel = match.group(1)
+		if buildLabel not in buildSet:
+			buildSet.add(buildLabel)
+			buildColor = colors[len(builds)]
+			builds.append({'label': buildLabel}, 'color': buildColor)
 
 		# Decompress files and convert to json
 		bytestream = open(fileLocation, "rb")
@@ -88,13 +130,10 @@ for filename in os.listdir(pathToExperimentFolder):
 			fin.write(text)
 		with open(newFile, 'r') as fin:
 			data = json.load(fin)
-			addDataPoints(data, isControl)
+			addDataPoints(data, label)
 
 # Graph first paint
-graphData(firstPaintList, "Time from first paint")
+graphData(firstPaintList, "simpleMeasurements.firstPaint", True)
 
 # Graph the about home top sites first paint
-graphData(firstPaintAboutHomeList, "Time from first paint about home top sites")
-
-# Graph the visible read bytes
-graphData(visibleReadBytes, "Read bytes from startup", "Number of Bytes (mb)")
+graphData(firstPaintAboutHomeList, "timestamps.about_home_topsites_first_paint", True)
